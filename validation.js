0471a260f81fb9d4d9c6a5fdcd14b98e39cf10b8
sahil0 <sahilmahale0@gmail.com>
2019-07-30 07:33:26 -0700

    Add files via upload
    You Have to Download and Open this in the same folder using any web Browser

---
+++ validation.js
@@ -0,0 +1,9 @@
+    function validates(){
+    var username = document.forms["Lform"]["Uname"];
+    var password1=document.forms["Lform"]["password1"];
+    var password2=document.forms["Lform"]["password2"];
+    var mnum=document.forms["Lform"]["Mobilno"];
+    
+    
+    
+}
